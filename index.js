6cf45dbe3b5e8464d2cb15d4aa016bb7e16795bd
Elaine <elainedoud@gmail.com>
2022-10-08 15:06:50 -0400

    commit tree 75a515a8f14d4ef28b2d91b6208f087d848d7bc7

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result;
+  if ( distance <= 400) {
+    result = 'This one is on me!';
+  }
+  
+  else if (distance > 400) {
+       if (distance <= 2000){
+        result = 'That will be twenty bucks.';
+      }
+      else if (distance > 2500) {
+        result = 'No can do.';
+      }
+      else if (distance > 2000) {
+        result = 'I will gladly take your thirty bucks.';
+      }
+      
+  } 
+    return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(nameCity){
+  return nameCity === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
